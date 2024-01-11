f885c17a0809990890d0563b8d6539f4184e0f46
IsaacBrandaoOSF <isaac.brandao@osf.digital>
2024-01-11 12:49:36 -0300

    feat(app): migrate data utils

--- src/utils/dataUtils.js
+++
@@ -1,4 +0,0 @@
-export const countValueOf = (dataArr, decimals) =>
-  Number(dataArr.reduce((total, { valor }) => total + Number(valor), 0).toFixed(decimals || 2));
-
-export const countValueOfByGroup = (dataArr, decimals) => dataArr.map((entries) => countValueOf(entries, decimals));
