bb504b4a7a5b6c81f72c09d1813ca03feda1e8ce
IsaacBrandaoOSF <isaac.brandao@osf.digital>
2024-02-29 17:01:37 -0300

    feat(nav-bar): hide navbar when user scroll down

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 
-import { Navbar, Container, Dropdown, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Dropdown } from 'react-bootstrap';
 import chartData from '../../data/database.json';
 import { getMonthName } from '../../utils/dateUtils';
 import { capitalizeFirstLetter } from '../../utils/stringUtils';
@@ -8,6 +8,8 @@ import { groupBy } from '../../utils/objectUtils';
 import { removeDuplicates } from '../../utils/arrayUtils';
 import { Link } from 'react-router-dom';
 import { SelectCallback } from '@restart/ui/esm/types';
+import { useScrollDirection } from 'react-use-scroll-direction';
+import classNames from 'classnames';
 import './NavBar.scss';
 
 interface INavBarProps {
@@ -19,6 +21,10 @@ interface INavBarProps {
 }
 
 const NavBar = ({ handleToggleMenu, handleYearChange, handleMonthChange, year, month }: INavBarProps) => {
+	const [isCollapsedNavbar, setIsCollapsedNavbar] = useState(false);
+	const [preventCollapse, setPreventCollapse] = useState(false);
+	const scrollDirection = useScrollDirection();
+
 	const availableYears = useMemo(
 		() => removeDuplicates(Object.keys(groupBy(chartData, 'pagamento')).map(date => date.split('/')[2])),
 		[chartData, groupBy, removeDuplicates],
@@ -29,21 +35,28 @@ const NavBar = ({ handleToggleMenu, handleYearChange, handleMonthChange, year, m
 		[chartData, groupBy, removeDuplicates],
 	);
 
-	// console.log(removeDuplicates(Object.keys(groupBy(chartData, "pagamento"))));
-
 	const [lastYear] = useMemo(() => availableYears.sort(), [availableYears]);
 	const [lastMonth] = useMemo(() => availableMonths.sort(), [availableMonths]);
 
-	{
-		/* <Nav onClick={handleToggleMenu}>
-  <Button bg="dark">
-    <i className="material-icons">menu</i>
-  </Button>
-</Nav> */
-	}
+	useEffect(() => {
+		if (!scrollDirection.isScrollingY) return;
+		if (preventCollapse) return;
+		setIsCollapsedNavbar(scrollDirection.isScrollingDown);
+		setPreventCollapse(true);
+
+		setTimeout(() => {
+			setPreventCollapse(false);
+		}, 500);
+	}, [scrollDirection]);
 
 	return (
-		<Navbar sticky="top" bg="dark" variant="dark" expand="lg" className="navbar-light">
+		<Navbar
+			className={classNames('navbar-light', 'CollapseNavbar', { show: !isCollapsedNavbar })}
+			sticky="top"
+			bg="dark"
+			variant="dark"
+			expand="lg"
+		>
 			<Container>
 				<Navbar.Brand>Relatório Financeiro</Navbar.Brand>
 
