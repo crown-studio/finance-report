0a3f6fceb2f44c351530a298bef94b9a2fe0705d
IsaacBrandaoOSF <isaac.brandao@osf.digital>
2024-05-11 23:31:22 -0300

    chore(constants): add categories constants

---
+++ src/constants/constantCategories.ts
@@ -0,0 +1,34 @@
+export const expenseCategories = [
+	'Ajuda Social',
+	'Alimentação',
+	'Eventos',
+	'Manutenção',
+	'Missões',
+	'Obras',
+	'Outros',
+	'Pagamentos',
+	'Serviços',
+	'Transporte',
+];
+
+export const expenseSubcategories = {
+	'Ajuda Social': ['Alimentação', 'Outros', 'Saúde', 'Transporte'],
+	Alimentação: ['Adultos', 'Comunhão', 'Crianças', 'Eventos', 'Jovens', 'Outros', 'Trabalhadores'],
+	Eventos: ['Batismo', 'Dia das Mães', 'Dia do Pastor', 'Dia dos Pais', 'Outros'],
+	Manutenção: ['Descartáveis', 'Limpeza', 'Outros', 'Papelaria', 'Utilidades'],
+	Missões: ['Igreja', 'Outros', 'Pessoal', 'Projeto', 'Sustento'],
+	Obras: ['Construção', 'Ferramentas', 'Material', 'Outros', 'Reforma', 'Reparos', 'Serviço', 'Outros'],
+	Outros: ['Outros'],
+	Pagamentos: ['Aluguel', 'Impostos', 'Outros', 'Salário', 'Taxas'],
+	Serviços: ['Contabilidade', 'Energia', 'Gás', 'Impressão', 'Outros', 'Água', 'Água Mineral'],
+	Transporte: ['Combustível', 'Manutenção', 'Outros', 'Taxi', 'Ônibus'],
+};
+
+export const revenueCategories = ['Dízimo', 'Oferta', 'Juros', 'Outros'];
+
+export const revenueSubcategories = {
+	Dízimo: ['EBD', 'Missões', 'Pessoal', 'Outros'],
+	Oferta: ['Outros'],
+	Juros: ['Outros'],
+	Outros: ['Outros'],
+};
